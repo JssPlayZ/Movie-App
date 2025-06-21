b7fe2e65eb0948ebd9daec6ff5060ec1a7bd4856
JssPlayZ <jatinshishodia123@gmail.com>
2025-06-21 19:28:24 +0530

    Favorites now works.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import MovieCard from "./components/MovieCard"
 import Favorites from './pages/favorites';
 import Home from './pages/Home';
 import {Routes, Route} from "react-router-dom"
+import { MovieProvider } from './contexts/movieContext';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
     <main className='main-content'>
       <Routes>
@@ -15,7 +16,7 @@ function App() {
          <Route path="/favorites" element={<Favorites />}/>
       </Routes>
     </main>
-    </div>
+    </MovieProvider>
   )
 }
 
